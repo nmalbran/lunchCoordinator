6e84785cadc4a6e14950eb7a07b6eb5198c26ddf
Nicolas Malbran <nmalbran@gmail.com>
2014-10-02 01:51:38 -0300

    Added USER/PASS settings for DB.

--- lunchCoordinator/settings.py
+++ lunchCoordinator/settings.py
@@ -61,6 +61,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'USER': '',
+        'PASSWORD': '',
     }
 }
 
